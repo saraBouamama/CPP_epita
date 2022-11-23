e662cfc31df53b4767772ec211baad6b629fa501
Thinkpad User <sara.bouamama@epita.fr>
2022-11-23 20:52:45 +0100

    added given files

---
+++ selection_sort_file/sort_file.hpp
@@ -0,0 +1,42 @@
+// Do not change
+#include <iostream>
+#include <fstream>
+
+
+#include <list>
+#include <vector>
+#include <deque>
+
+#include "my_sort.hpp"
+
+// Change here
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+
+XXX
+void read_file_to_cont(XXX& f, XXX c) {
+  XXX
+}
+
+XXX
+void read_and_sort(XXX& fi, XXX& fo) {
+  XXX
+}
+
+XXX
+void read_and_sort_decide_container(XXX& fi, XXX& fo) {
+  XXX
+  switch(XXX)
+  {
+    XXX
+  }
+}
+
+void read_and_sort_decide_valuetype(XXX& fi, XXX& fo) {
+  XXX
+  switch(XXX)
+  {
+    XXX
+  }
+}
